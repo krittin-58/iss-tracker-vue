08beef41fef3868603b3c3cc675634738d30e922
Krittin Jaruvisut <krittin.j58@rsu.ac.th>
2020-05-22 14:58:36 +0700

    fixed cors and added getPassTimeISS

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
-const API_URL = 'http://api.open-notify.org/';
+const API_URL = 'http://api.open-notify.org';
+
+const CORS_URL = 'https://cors-anywhere.herokuapp.com';
 
 const ApiService = {
   // eslint-disable-next-line class-methods-use-this
   getCurrentLocationISS() {
-    const url = `${API_URL}/iss-now.json`;
+    const url = `${CORS_URL}/${API_URL}/iss-now.json`;
+    return axios.get(url);
+  },
+
+  getPassTimeISS(LAT, LON) {
+    const url = `${CORS_URL}/${API_URL}/iss-pass.json?lat=${LAT}&lon=${LON}`;
     return axios.get(url);
   },
 };
--- src/views/Passtime.vue
+++ src/views/Passtime.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <h2>{{ text.headline }}</h2>
+    <h3>{{ passtime }}</h3>
+    <h4>{{ error }}</h4>
   </div>
 </template>
 
 <script>
+import ApiService from '../services/api';
+
 export default {
   name: 'PassTime-ISS',
   data() {
@@ -12,7 +16,27 @@ export default {
       text: {
         headline: 'ISS Pass Time',
       },
+      locations: {
+        lat: '48.4809',
+        lon: '-167.7702',
+      },
+      passtime: null,
+      error: '',
     };
+  },
+
+  mounted() {
+    this.getPassTime();
+  },
+
+  methods: {
+    getPassTime() {
+      ApiService.getPassTimeISS(this.locations.lat, this.locations.lon).then((data) => {
+        this.passtime = data.data.request;
+      }).catch((error) => {
+        this.error = error;
+      });
+    },
   },
 };
 </script>

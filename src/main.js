b39e2aea10039c912830a6545fbf7fd6e9622320
Krittin Jaruvisut <krittin.j58@rsu.ac.th>
2020-05-26 17:40:10 +0700

    deleted Map.vue and fixed some thing

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
+import x5GMaps from 'x5-gmaps';
 import App from './App.vue';
 import router from './router';
+
+
+Vue.use(x5GMaps, 'AIzaSyBs1AwgEI7aFQ-vimkQv4cHUQkmWiqmdDY');
 
 Vue.config.productionTip = false;
 
